bf8d57e04dfac83bfe1c1c488094003b77aba01b
bansheerubber <gary.cactus.email@gmail.com>
2022-11-13 15:38:36 -0700

    streams: decode to a specific type instead of a trait

--- streams/src/read_stream.rs
+++ streams/src/read_stream.rs
@@ -1,16 +1,15 @@
-use crate::Decode;
-
 pub type StreamPosition = u64;
 pub type StreamPositionDelta = u64;
 
 /// Stream that imports in data with the specified `Encoding`, and can decode the data into Rust objects.
 pub trait ReadStream<Encoding>: Sized {
+	type DecodedType;
 	type Error: std::fmt::Debug;
 	type Import;
 
 	/// Use the `Decode` trait to decode an object out of the stream. Consumes the stream until `can_decode` returns
 	/// false.
-	fn decode<T: Decode<Encoding, Self>>(&mut self) -> T;
+	fn decode(&mut self) -> Self::DecodedType;
 
 	/// Whether or not we have enough data to decode.
 	fn can_decode(&self) -> bool;
