c1919f88dcecf6ee79564fd180d206826e23bd01
bansheerubber <gary.cactus.email@gmail.com>
2022-11-12 23:30:36 -0700

    carton: work on `Carton` decode & add documentation

--- carton/src/bin.rs
+++ carton/src/bin.rs
@@ -5,17 +5,10 @@ use carton::stream::Decode;
 
 fn main() {
 	// let mut carton = Carton::default();
-	// carton.string_table.insert("test1");
-	// carton.string_table.insert("test2");
-	// carton.string_table.insert("test3");
-	// carton.string_table.insert("test4");
-	// carton.string_table.insert("test5");
+	// carton.add_directory("scratch/resources");
+	// carton.to_file("scratch/resources.carton");
 
-	// carton.add_file("scratch/test/file.txt");
-
-	// carton.to_file("scratch/test.carton");
-
-	let mut file = std::fs::File::open("scratch/test.carton").unwrap();
+	let mut file = std::fs::File::open("scratch/resources.carton").unwrap();
 	let mut data = Vec::new();
 	file.read_to_end(&mut data).unwrap();
 
--- carton/src/carton.rs
+++ carton/src/carton.rs
@@ -1,18 +1,28 @@
+use walkdir::WalkDir;
+
 use crate::file::File;
 use crate::file_table::FileTable;
 use crate::stream::{ Decode, Encode, EncodeMut, Stream, };
 use crate::stream::reading::{ read_char, read_u8, read_u64, };
 use crate::stream::writing::{ write_char, write_u8, write_u64, };
 use crate::StringTable;
-use crate::translation_layer::FileEncoder;
+use crate::translation_layer::{ FileDecoder, FileEncoder, };
 
 const CARTON_VERSION: u8 = 2;
 
-/// Representation of a carton file.
+/// Representation of a carton. Cartons are an archive file format designed for efficient storage of video game data.
+/// Features include compression of files, a metadata database for looking up files during runtime, and streaming files
+/// in discrete chunks for audio streams. The carton file format is built upon a stream encoding/decoding API that is
+/// designed to support everything from storing data in files, to sending data over the network. Cartons are designed to
+/// be constructed from a directory which includes data to be included in a video game. The carton preserves the file
+/// structure and automatically assigns imported files with metadata read from TOML files.
 #[derive(Debug)]
 pub struct Carton {
+	/// Keeps track of files in the carton.
 	pub(crate) file_table: FileTable,
+	/// Stores strings used throughout the carton.
 	pub string_table: StringTable,
+	/// Version of the carton encoding.
 	pub version: u8,
 }
 
@@ -38,8 +48,29 @@ impl Carton {
 	pub fn add_file(&mut self, file_name: &str) {
 		self.file_table.add_from_disk(File::from_file(file_name).unwrap());
 	}
+
+	/// Add a directory to the carton. All files in the directory will be added into the carton.
+	pub fn add_directory(&mut self, directory_name: &str) {
+		for entry in WalkDir::new(directory_name) {
+			let entry = entry.unwrap();
+			if entry.metadata().unwrap().is_file() {
+				let file_name = entry.path().to_str().unwrap();
+				if file_name.contains("metadata") {
+					continue;
+				}
+
+				self.add_file(file_name);
+			}
+		}
+	}
 }
 
+/// Encode the carton into a `.carton` file. Carton files start with the `CARTON` magic number and the carton encoding
+/// version. The encoding reserves a 8 byte number after the version that will point to the file table. Files and their
+/// metadata are written first. The file and metadata encoding process updates the internal state of the carton,
+/// necessary for completing the file and string tables. Once all files are written, the file table is written with the
+/// string table following afterwards. Once the file table and string tables are written, the file table pointer at the
+/// start of the file is updated to point to the absolute location of the file table.
 impl EncodeMut for Carton {
 	fn encode_mut(&mut self, vector: &mut Vec<u8>) {
 		// write magic number and the version
@@ -59,16 +90,17 @@ impl EncodeMut for Carton {
 		for file in self.file_table.get_files() {
 			let mut encoder = FileEncoder {
 				file,
-				position: 0,
+				file_position: 0,
+				metadata_position: 0,
 				string_table: &mut self.string_table,
 			};
 
 			encoder.encode_mut(vector);
-			positions.push((String::from(file.get_file_name()), encoder.position));
+			positions.push((String::from(file.get_file_name()), encoder.metadata_position, encoder.file_position));
 		}
 
-		for (file_name, position) in positions {
-			self.file_table.update_position(&file_name, position);
+		for (file_name, metadata_position, file_position) in positions {
+			self.file_table.update_position(&file_name, metadata_position, file_position);
 		}
 
 		let file_table_position = vector.len() as u64;
@@ -84,11 +116,17 @@ impl EncodeMut for Carton {
 	}
 }
 
+/// Decode a `.carton` file. Carton files first check the file's magic number and carton encoding version, and then
+/// move onto decoding the file table and string table. Afterwards, all files and their metadata are decoded. The only
+/// data loaded into memory are the representations of the files, instead of their entire contents. The API gives the
+/// user the option to load the file into memory after decoding. Metadata is always memory resident, since the API gives
+/// a way to query files in the carton by searching metadata values.
 impl Decode for Carton {
 	fn decode(vector: &[u8]) -> (Self, &[u8]) {
 		let mut magic_number_pointer = vector;
 		let mut carton = Carton::default();
 
+		// check the carton magic number
 		let mut magic = String::new();
 		for _ in 0..6 {
 			let (character, new_position) = read_char(magic_number_pointer);
@@ -100,6 +138,7 @@ impl Decode for Carton {
 			panic!("Invalid magic number");
 		}
 
+		// check the carton version
 		let (version, new_position) = read_u8(magic_number_pointer);
 		magic_number_pointer = new_position;
 		if version != CARTON_VERSION {
@@ -108,11 +147,23 @@ impl Decode for Carton {
 
 		let (file_table_pointer, _) = read_u64(magic_number_pointer);
 
+		// partially load the file table, only file name -> metadata position map is valid
 		let (file_table, new_position) = FileTable::decode(&vector[(file_table_pointer as usize)..]);
 		carton.file_table = file_table;
 
+		// fully load the string table
 		let (string_table, _) = StringTable::decode(new_position);
 		carton.string_table = string_table;
+
+		// load metadata and files from carton
+		let mut files = Vec::new();
+		for (name,  position) in carton.file_table.get_metadata_positions() {
+			let (file, _) = FileDecoder::decode(&vector[(*position as usize)..]);
+			files.push(file);
+		}
+
+		// finish off loading the file table
+		println!("{:?}", files);
 
 		return (carton, vector);
 	}
