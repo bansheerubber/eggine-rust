59c2e485432e4c00bbdd24f3f60d1cb4bb0dbd60
bansheerubber <gary.cactus.email@gmail.com>
2022-11-12 23:31:41 -0700

    carton: separate out metadata and file positions

--- carton/src/file_table.rs
+++ carton/src/file_table.rs
@@ -8,8 +8,17 @@ use crate::stream::writing::{ write_string, write_u64 };
 /// Maps files to their absolute positions within the carton.
 #[derive(Debug, Default)]
 pub(crate) struct FileTable {
+	/// List of files.
 	files: Vec<File>,
-	positions: HashMap<String, u64>,
+	/// Where the file metadata is located. File metadata is positioned before file contents. If we're building a carton
+	/// from a directory, then the metadata mapping is only valid during file encoding and remains empty beforehand. If
+	/// we're importing a carton from a `.carton` file, then the metadata mapping becomes valid immediately after file
+	/// table decoding.
+	metadata_positions: HashMap<String, u64>,
+	/// Where the file data begins. Represents byte 0 of the file. If we're building a carton from a directory, then the
+	/// absolute position mapping is only valid during file encoding and remains empty beforehand. If we're importing a
+	/// carton from a `.carton` file, then the metadata mapping becomes valid only after we decode the entire carton.
+	file_positions: HashMap<String, u64>,
 }
 
 impl FileTable {
@@ -18,30 +27,44 @@ impl FileTable {
 		self.files.push(file);
 	}
 
-	pub fn update_position(&mut self, file_name: &str, position: u64) {
-		self.positions.insert(String::from(file_name), position);
+	/// Add a file's metadata position into the table during the decode process.
+	pub(crate) fn add_position_from_decode(&mut self, file_name: String, metadata_position: u64) {
+		self.metadata_positions.insert(file_name.clone(), metadata_position);
 	}
 
+	/// Update the file's metadata position and absolute file position.
+	pub fn update_position(&mut self, file_name: &str, metadata_position: u64, file_position: u64) {
+		self.metadata_positions.insert(String::from(file_name), metadata_position);
+		self.file_positions.insert(String::from(file_name), file_position);
+	}
+
+	/// Get the list of files the table keeps track of.
 	pub fn get_files(&self) -> &Vec<File> {
 		&self.files
 	}
 
-	pub(crate) fn add_from_decode(&mut self, file_name: String, position: u64) {
-		self.positions.insert(file_name, position);
+	/// Get a reference for file metadata positions.
+	pub fn get_metadata_positions(&self) -> &HashMap<String, u64> {
+		&self.metadata_positions
 	}
 }
 
+/// The file table stores a key/value pair where the keys are file names and the values are file metadata positions. If
+/// we follow the position pointer, we will read either the encoded TOML metadata for the file, or if lacking TOML
+/// metadata, the compression level, file size, and file name.
 impl Encode for FileTable {
 	fn encode(&self, vector: &mut Vec<u8>) {
-		write_u64(self.positions.len() as u64, vector);
+		write_u64(self.metadata_positions.len() as u64, vector);
 
-		for (file_name, position) in self.positions.iter() {
+		for (file_name, position) in self.metadata_positions.iter() {
 			write_string(file_name, vector);
 			write_u64(*position, vector);
 		}
 	}
 }
 
+/// We can only resolve metadata positions at this point. We have to do a separate pass for file starting positions
+/// after the initial decode. File starting positions are inferred during metadata decoding.
 impl Decode for FileTable {
 	fn decode(vector: &[u8]) -> (Self, &[u8]) {
 		let mut vector = vector;
@@ -57,7 +80,7 @@ impl Decode for FileTable {
 			let (file_position, new_position) = read_u64(vector);
 			vector = new_position;
 
-			table.add_from_decode(file_name, file_position);
+			table.add_position_from_decode(file_name, file_position);
 		}
 
 		return (table, vector);
