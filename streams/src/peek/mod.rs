60f12ed1c724166c1891513865880b862f1d2686
bansheerubber <gary.cactus.email@gmail.com>
2022-11-14 19:48:44 -0700

    streams: update documentation for `Seekable` & `Peekable`

--- streams/src/peek/mod.rs
+++ streams/src/peek/mod.rs
@@ -1,3 +1,4 @@
+/// Allow a stream to peek a primitive at it's current read/write position.
 pub trait Peekable<Encoding> {
 	fn peek(&mut self) -> Encoding;
 }
--- streams/src/seek/mod.rs
+++ streams/src/seek/mod.rs
@@ -1,5 +1,6 @@
 use crate::StreamPosition;
 
+/// Allow a stream to seek to a position within the stream.
 pub trait Seekable {
 	fn seek(&mut self, position: StreamPosition);
 	fn get_position(&mut self) -> StreamPosition;
