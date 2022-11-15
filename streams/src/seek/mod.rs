625e4c46b17f69a1e201b1d8d517fb73ba353a32
bansheerubber <gary.cactus.email@gmail.com>
2022-11-14 19:33:13 -0700

    streams: allow seekable to mutate on `get_position` call

--- streams/src/seek/mod.rs
+++ streams/src/seek/mod.rs
@@ -2,5 +2,5 @@ use crate::StreamPosition;
 
 pub trait Seekable {
 	fn seek(&mut self, position: StreamPosition);
-	fn get_position(&self) -> StreamPosition;
+	fn get_position(&mut self) -> StreamPosition;
 }
