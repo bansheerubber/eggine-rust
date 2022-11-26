2aa2d8b6109428a1bb2009582079827e95ac94ae
bansheerubber <gary.cactus.email@gmail.com>
2022-11-26 12:08:00 -0700

    network: `NtpServer` uses packet headers & distinguishes packet types

--- network/src/server/ntp/server.rs
+++ network/src/server/ntp/server.rs
@@ -9,7 +9,7 @@ use tokio::net::{ ToSocketAddrs, UdpSocket, };
 use crate::error::NetworkStreamError;
 use crate::log::{ Log, LogLevel, };
 use crate::network_stream::NetworkReadStream;
-use crate::payload::NtpClientPacket;
+use crate::payload::{ NtpPacketHeader, NtpRequestPacket, };
 
 use super::{ MAX_NTP_PACKET_SIZE, NTP_MAGIC_NUMBER, };
 
@@ -21,6 +21,8 @@ pub enum NtpServerError {
 	InvalidIP,
 	/// Emitted if the client did not send us the expected magic number.
 	InvalidMagicNumber(SocketAddr),
+	/// Emitted if the client sent us a packet type that we do not recognize
+	InvalidPacketType(SocketAddr),
 	/// Emitted if we encountered a problem with the tokio mpsc UDP socket communication.
 	MpscError(mpsc::error::TryRecvError),
 	/// Emitted if we encountered a problem with network streams.
@@ -41,6 +43,7 @@ impl NtpServerError {
 		match *self {
 			NtpServerError::Create(_) => true,
 			NtpServerError::InvalidIP => false,
+			NtpServerError::InvalidPacketType(_) => false,
 			NtpServerError::InvalidMagicNumber(_) => false,
 			NtpServerError::MpscError(error) => {
 				if let mpsc::error::TryRecvError::Disconnected = error {
@@ -78,7 +81,6 @@ pub struct NtpServer {
 	address: SocketAddr,
 	/// The IPV6 addresses that are allowed to communicate with the NTP server.
 	pub address_whitelist: HashSet<Ipv6Addr>,
-	expected_client_packet: NtpClientPacket,
 	log: Log,
 	/// Amount of time it takes to read system time, in nanoseconds.
 	precision: u64,
@@ -124,10 +126,6 @@ impl NtpServer {
 		Ok(NtpServer {
 			address: socket.local_addr().unwrap(),
 			address_whitelist: HashSet::new(),
-			expected_client_packet: NtpClientPacket {
-				index: 0,
-				magic_number: String::from(NTP_MAGIC_NUMBER),
-			},
 			precision: (total / BENCHMARK_TIMES) as u64,
 			log: Log::default(),
 			receive_stream: NetworkReadStream::new(),
@@ -145,6 +143,84 @@ impl NtpServer {
 				Err(error) => return Err(error.into()),
 			}
 		}
+	}
+
+	/// Process a NTP timing information request. Send the timing information back to the socket.
+	async fn process_request(&mut self, source: SocketAddr, recv_time: i128) -> Result<(), NtpServerError> {
+		let client_packet = self.receive_stream.decode::<NtpRequestPacket>()?.0;
+
+		// send the server time back
+		let mut buffer: [u8; 51] = [0; 51];
+
+		// write the NTP packet header
+		buffer[0] = 'E' as u8;
+		buffer[1] = 'G' as u8;
+		buffer[2] = 'G' as u8;
+		buffer[3] = 'I' as u8;
+		buffer[4] = 'N' as u8;
+		buffer[5] = 'E' as u8;
+		buffer[6] = 'N' as u8;
+		buffer[7] = 'T' as u8;
+		buffer[8] = 'P' as u8;
+		buffer[9] = 1;
+
+		buffer[10] = client_packet.index;
+
+		// we need to send the times as quick as possible since the longer we take, the more inaccurate the `send_time` is
+		// going to be. sends two 128 bit integers for receive time and send time.
+		buffer[11] = (recv_time & 0xFF) as u8;
+		buffer[12] = ((recv_time >> 8) & 0xFF) as u8;
+		buffer[13] = ((recv_time >> 16) & 0xFF) as u8;
+		buffer[14] = ((recv_time >> 24) & 0xFF) as u8;
+		buffer[15] = ((recv_time >> 32) & 0xFF) as u8;
+		buffer[16] = ((recv_time >> 40) & 0xFF) as u8;
+		buffer[17] = ((recv_time >> 48) & 0xFF) as u8;
+		buffer[18] = ((recv_time >> 56) & 0xFF) as u8;
+		buffer[19] = ((recv_time >> 64) & 0xFF) as u8;
+		buffer[20] = ((recv_time >> 72) & 0xFF) as u8;
+		buffer[21] = ((recv_time >> 80) & 0xFF) as u8;
+		buffer[22] = ((recv_time >> 88) & 0xFF) as u8;
+		buffer[23] = ((recv_time >> 96) & 0xFF) as u8;
+		buffer[24] = ((recv_time >> 104) & 0xFF) as u8;
+		buffer[25] = ((recv_time >> 112) & 0xFF) as u8;
+		buffer[26] = ((recv_time >> 120) & 0xFF) as u8;
+
+		// send precision
+		buffer[27] = (self.precision & 0xFF) as u8;
+		buffer[28] = ((self.precision >> 8) & 0xFF) as u8;
+		buffer[29] = ((self.precision >> 16) & 0xFF) as u8;
+		buffer[30] = ((self.precision >> 24) & 0xFF) as u8;
+		buffer[31] = ((self.precision >> 32) & 0xFF) as u8;
+		buffer[32] = ((self.precision >> 40) & 0xFF) as u8;
+		buffer[33] = ((self.precision >> 48) & 0xFF) as u8;
+		buffer[34] = ((self.precision >> 56) & 0xFF) as u8;
+
+		let send_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros() as i128;
+
+		// send time encoding
+		buffer[35] = (send_time & 0xFF) as u8;
+		buffer[36] = ((send_time >> 8) & 0xFF) as u8;
+		buffer[37] = ((send_time >> 16) & 0xFF) as u8;
+		buffer[38] = ((send_time >> 24) & 0xFF) as u8;
+		buffer[39] = ((send_time >> 32) & 0xFF) as u8;
+		buffer[40] = ((send_time >> 40) & 0xFF) as u8;
+		buffer[41] = ((send_time >> 48) & 0xFF) as u8;
+		buffer[42] = ((send_time >> 56) & 0xFF) as u8;
+		buffer[43] = ((send_time >> 64) & 0xFF) as u8;
+		buffer[44] = ((send_time >> 72) & 0xFF) as u8;
+		buffer[45] = ((send_time >> 80) & 0xFF) as u8;
+		buffer[46] = ((send_time >> 88) & 0xFF) as u8;
+		buffer[47] = ((send_time >> 96) & 0xFF) as u8;
+		buffer[48] = ((send_time >> 104) & 0xFF) as u8;
+		buffer[49] = ((send_time >> 112) & 0xFF) as u8;
+		buffer[50] = ((send_time >> 120) & 0xFF) as u8;
+
+		// TODO check if the amount of bytes sent in the socket matches the size of the exported vector
+		if let Err(error) = self.socket.send_to(&buffer, source).await {
+			return Err(NtpServerError::Send(error));
+		}
+
+		Ok(())
 	}
 
 	async fn process(&mut self, message: Message) -> Result<(), NtpServerError> {
@@ -175,70 +251,19 @@ impl NtpServer {
 
 		self.receive_stream.import(buffer)?;
 
-		let client_packet = self.receive_stream.decode::<NtpClientPacket>()?.0;
-		self.expected_client_packet.index = client_packet.index;
-		if client_packet != self.expected_client_packet {
+		let packet_header = self.receive_stream.decode::<NtpPacketHeader>()?.0;
+		if packet_header.magic_number != NTP_MAGIC_NUMBER {
 			self.log.print(LogLevel::Error, format!("received invalid magic number from {:?}", source), 0);
 			return Err(NtpServerError::InvalidMagicNumber(source));
 		}
 
-		// send the server time back
-		let mut buffer: [u8; 41] = [0; 41];
-
-		buffer[0] = client_packet.index;
-
-		// we need to send the times as quick as possible since the longer we take, the more inaccurate the `send_time` is
-		// going to be. sends two 128 bit integers for receive time and send time.
-		buffer[1] = (recv_time & 0xFF) as u8;
-		buffer[2] = ((recv_time >> 8) & 0xFF) as u8;
-		buffer[3] = ((recv_time >> 16) & 0xFF) as u8;
-		buffer[4] = ((recv_time >> 24) & 0xFF) as u8;
-		buffer[5] = ((recv_time >> 32) & 0xFF) as u8;
-		buffer[6] = ((recv_time >> 40) & 0xFF) as u8;
-		buffer[7] = ((recv_time >> 48) & 0xFF) as u8;
-		buffer[8] = ((recv_time >> 56) & 0xFF) as u8;
-		buffer[9] = ((recv_time >> 64) & 0xFF) as u8;
-		buffer[10] = ((recv_time >> 72) & 0xFF) as u8;
-		buffer[11] = ((recv_time >> 80) & 0xFF) as u8;
-		buffer[12] = ((recv_time >> 88) & 0xFF) as u8;
-		buffer[13] = ((recv_time >> 96) & 0xFF) as u8;
-		buffer[14] = ((recv_time >> 104) & 0xFF) as u8;
-		buffer[15] = ((recv_time >> 112) & 0xFF) as u8;
-		buffer[16] = ((recv_time >> 120) & 0xFF) as u8;
-
-		// send precision
-		buffer[32] = (self.precision & 0xFF) as u8;
-		buffer[33] = ((self.precision >> 8) & 0xFF) as u8;
-		buffer[34] = ((self.precision >> 16) & 0xFF) as u8;
-		buffer[35] = ((self.precision >> 24) & 0xFF) as u8;
-		buffer[36] = ((self.precision >> 32) & 0xFF) as u8;
-		buffer[37] = ((self.precision >> 40) & 0xFF) as u8;
-		buffer[38] = ((self.precision >> 48) & 0xFF) as u8;
-		buffer[39] = ((self.precision >> 56) & 0xFF) as u8;
-
-		let send_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros() as i128;
-
-		// send time encoding
-		buffer[17] = (send_time & 0xFF) as u8;
-		buffer[18] = ((send_time >> 8) & 0xFF) as u8;
-		buffer[19] = ((send_time >> 16) & 0xFF) as u8;
-		buffer[20] = ((send_time >> 24) & 0xFF) as u8;
-		buffer[21] = ((send_time >> 32) & 0xFF) as u8;
-		buffer[22] = ((send_time >> 40) & 0xFF) as u8;
-		buffer[23] = ((send_time >> 48) & 0xFF) as u8;
-		buffer[24] = ((send_time >> 56) & 0xFF) as u8;
-		buffer[25] = ((send_time >> 64) & 0xFF) as u8;
-		buffer[26] = ((send_time >> 72) & 0xFF) as u8;
-		buffer[27] = ((send_time >> 80) & 0xFF) as u8;
-		buffer[28] = ((send_time >> 88) & 0xFF) as u8;
-		buffer[29] = ((send_time >> 96) & 0xFF) as u8;
-		buffer[30] = ((send_time >> 104) & 0xFF) as u8;
-		buffer[31] = ((send_time >> 112) & 0xFF) as u8;
-		buffer[32] = ((send_time >> 120) & 0xFF) as u8;
-
-		// TODO check if the amount of bytes sent in the socket matches the size of the exported vector
-		if let Err(error) = self.socket.send_to(&buffer, source).await {
-			return Err(NtpServerError::Send(error));
+		match packet_header.packet_type {
+			0 => self.process_request(source, recv_time).await?,
+			1 => {},
+			_ => {
+				self.log.print(LogLevel::Error, format!("received invalid packet type from {:?}", source), 0);
+				return Err(NtpServerError::InvalidPacketType(source));
+			}
 		}
 
 		Ok(())
