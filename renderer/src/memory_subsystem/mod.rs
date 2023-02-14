b0a5dff17be38f479ecaab69d2bc58ff5bb2d04b
bansheerubber <gary.cactus.email@gmail.com>
2023-02-13 17:24:35 -0700

    renderer: create concept of `ShapeBlueprintState`

--- renderer/src/memory_subsystem/mod.rs
+++ renderer/src/memory_subsystem/mod.rs
@@ -6,4 +6,5 @@ pub use memory::Memory;
 pub use node::Node;
 pub use node::NodeKind;
 pub use page::Page;
+pub use page::PageError;
 pub use page::PageUUID;
--- renderer/src/shape/shape_blueprint.rs
+++ renderer/src/shape/shape_blueprint.rs
@@ -8,9 +8,55 @@ use fbxcel_dom::any::AnyDocument;
 use fbxcel_dom::v7400::object::TypedObjectHandle;
 use fbxcel_dom::v7400::object::model::TypedModelHandle;
 
-use crate::memory_subsystem::{ Memory, Node, NodeKind, };
+use crate::memory_subsystem::{ Memory, Node, NodeKind, PageError, };
 use crate::shape::triangulator::triangulator;
-use super::shape_buffer::ShapeBuffer;
+
+/// Controls how a `ShapeBlueprint` allocates memory and helps with calculating `first_index` and `vertex_offset`
+/// properties for `Mesh`s
+pub trait ShapeBlueprintState {
+	/// Calculates the first index of the mesh, which a `Mesh` uses to index into the index buffer.
+	///
+	/// * `num_indices` - The amount of index values written in the index buffer. Additional disambiguation: index values
+	/// are repeated in the index buffer, and `num_indices` includes all the repeats since it is the total number of
+	/// indices written into the index buffer.
+	fn calc_first_index(&mut self, num_indices: u32) -> u32;
+
+	/// Calculates the vertex offset of the mesh, which a 'Mesh' uses to calculate the base vertex index used to index
+	/// into the vertex buffer.
+	///
+	/// * `last_highest_index` - The highest index value from the mesh that the `vertex_offset` will be assigned to. If an
+	/// index buffer uses numbers within the range 0..=32, then the `highest_index` passed to this function should be
+	/// `32`.
+	fn calc_vertex_offset(&mut self, highest_index: i32) -> i32;
+
+	/// Prepares memory for the next mesh.
+	fn prepare_mesh_pages(&mut self);
+
+	/// Gets the node that the blueprint will store information into.
+	///
+	/// * `name`      - A descriptor for the kind of data stored in the node. Vec3 vertex information would be stored in a
+	/// separate node from Vec4 color information, with the correct node specified by `name`. The `name` does not describe
+	/// the GLSL type of data stored, so additional parameters are necessary for node allocation.
+	/// * `size`      - The expected size of the returned node.
+	/// * `align`     - The expected alignment of the returned node.
+	/// * `node_kind` - The expected kind of the returned node.
+	///
+	/// # Return value
+	/// Since an implementation of this function may allocate new nodes, a `PageError` is returned so the blueprint can
+	/// handle them gracefully. The onus of pretty-printing memory error debug is on callers.
+	/// If the unwrapped result is `None`, then the `ShapeBlueprintState` implementation does not support storing the kind
+	/// of data described by `name`, and `ShapeBlueprint` should throw away any such data it loaded.
+	fn get_named_node(
+		&self,
+		name: ShapeBlueprintDataKind,
+		size: u64,
+		align: u64,
+		node_kind: NodeKind,
+	) -> Result<Option<Node>, PageError>;
+
+	/// Wrapper function for writing data into the specified node.
+	fn write_node(&mut self, node: &Node, buffer: Vec<u8>);
+}
 
 #[derive(Debug)]
 pub enum ShapeBlueprintError {
@@ -27,13 +73,22 @@ pub struct Mesh {
 	/// Used for indirect rendering.
 	pub first_index: u32,
 	/// Points to the mesh's vertex indices. Indices are u16s.
-	indices: Node,
+	indices: Option<Node>,
 	/// Points to the mesh's vertex vec3 data. Vertices are f32s.
-	vertices: Node,
+	vertices: Option<Node>,
 	/// The amount of vertices in the mesh.
 	pub vertex_count: u32,
 	/// Used for indirect rendering.
 	pub vertex_offset: i32,
+}
+
+/// Specifies the kind of data that was loaded from a shape file. Used to communicate what data `ShapeBlueprint` wants
+/// to store using the `ShapeBlueprintState` trait.
+#[derive(Debug)]
+pub enum ShapeBlueprintDataKind {
+	Esoteric(String),
+	Index,
+	Vertex,
 }
 
 /// A collection of meshes loaded from a single FBX file.
@@ -46,7 +101,7 @@ pub struct ShapeBlueprint {
 impl ShapeBlueprint {
 	/// Load a FBX file from a carton.
 	pub fn load(
-		file_name: &str, carton: &mut Carton, memory: Arc<RwLock<Memory>>, shape_buffer: &mut ShapeBuffer,
+		file_name: &str, carton: &mut Carton, state: &mut dyn ShapeBlueprintState
 	) -> Result<Rc<ShapeBlueprint>, ShapeBlueprintError> {
 		// load the FBX up from the carton
 		let fbx_stream = match carton.get_file_data(file_name) {
@@ -102,26 +157,40 @@ impl ShapeBlueprint {
 			}
 		}
 
-		let mut memory = memory.write().unwrap();
-
 		// go through the mesh data and create nodes for it
 		let mut mesh_representations = Vec::new();
 		for (vertices, indices) in meshes.iter() {
 			let vertex_count = indices.len() as u32; // amount of vertices to render
 
 			// allocate node for `Vec3` vertices
-			let vertices = memory.get_page_mut(shape_buffer.vertex_page).unwrap().allocate_node(
+			let vertices = state.get_named_node(
+				ShapeBlueprintDataKind::Vertex,
 				(vertices.len() * 3 * std::mem::size_of::<f32>()) as u64,
 				(3 * std::mem::size_of::<f32>()) as u64,
 				NodeKind::Buffer
-			).unwrap();
+			)
+				.or_else(
+					|_| -> Result<Option<Node>, ()> {
+						eprintln!("Could not allocate node for {:?}", ShapeBlueprintDataKind::Vertex);
+						Ok(None)
+					}
+				)
+				.unwrap();
 
-			// allocate node for `u32` indices
-			let indices = memory.get_page_mut(shape_buffer.index_page).unwrap().allocate_node(
+			// allocate node for `u32` Indices
+			let indices = state.get_named_node(
+				ShapeBlueprintDataKind::Index,
 				(indices.len() * std::mem::size_of::<u32>()) as u64,
 				std::mem::size_of::<u32>() as u64,
 				NodeKind::Buffer
-			).unwrap();
+			)
+				.or_else(
+					|_| -> Result<Option<Node>, ()> {
+						eprintln!("Could not allocate node for {:?}", ShapeBlueprintDataKind::Index);
+						Ok(None)
+					}
+				)
+				.unwrap();
 
 			// push the mesh representation
 			mesh_representations.push(Mesh {
@@ -134,38 +203,42 @@ impl ShapeBlueprint {
 		}
 
 		// schedule buffer writes
-		let mut index_count = 0;
+		let mut num_indices = 0;
 		let mut highest_index = 0;
 		for ((vertices, indices), mesh) in meshes.iter().zip(mesh_representations.iter_mut()) {
 			// serialize vertices & write to buffer
-			let mut u8_vertices: Vec<u8> = Vec::new();
-			for point in vertices {
-				u8_vertices.extend_from_slice(bytemuck::bytes_of(point));
-			}
+			if let Some(vertices_node) = &mesh.vertices {
+				let mut u8_vertices: Vec<u8> = Vec::new();
+				for point in vertices {
+					u8_vertices.extend_from_slice(bytemuck::bytes_of(point));
+				}
 
-			memory.write_buffer(shape_buffer.vertex_page, &mesh.vertices, u8_vertices);
+				state.write_node(&vertices_node, u8_vertices);
+			}
 
 			// serialize indices & write to buffer
-			let mut u8_indices: Vec<u8> = Vec::new();
-			for index in indices {
-				highest_index = std::cmp::max(highest_index, *index);
-				u8_indices.extend_from_slice(bytemuck::bytes_of(index));
+			if let Some(indices_node) = &mesh.indices {
+				let mut u8_indices: Vec<u8> = Vec::new();
+				for index in indices {
+					highest_index = std::cmp::max(highest_index, *index);
+					u8_indices.extend_from_slice(bytemuck::bytes_of(index));
+				}
+
+				state.write_node(&indices_node, u8_indices);
 			}
 
-			index_count += indices.len();
+			num_indices += indices.len();
 
 			// take the first_index from the indices_written property, then accumulate
-			let first_index = shape_buffer.indices_written;
-			shape_buffer.indices_written += index_count as u32;
+			// let first_index = shape_buffer.indices_written;
+			// shape_buffer.indices_written += index_count as u32;
 
 			// take the vertex_offset from the highest_vertex_offset property, then accumulate
-			let vertex_offset = shape_buffer.highest_vertex_offset;
-			shape_buffer.highest_vertex_offset += highest_index as i32;
+			// let vertex_offset = shape_buffer.highest_vertex_offset;
+			// shape_buffer.highest_vertex_offset += highest_index as i32;
 
-			mesh.first_index = first_index;
-			mesh.vertex_offset = vertex_offset;
-
-			memory.write_buffer(shape_buffer.index_page, &mesh.indices, u8_indices);
+			mesh.first_index = state.calc_first_index(num_indices as u32);
+			mesh.vertex_offset = state.calc_vertex_offset(highest_index as i32);
 		}
 
 		Ok(Rc::new(ShapeBlueprint {
