7dd1cf53b94fa16e2916a82e39ccbbca8d26c0ab
bansheerubber <gary.cactus.email@gmail.com>
2023-02-14 00:32:44 -0700

    renderer: add uniform

--- renderer/src/testing/mod.rs
+++ renderer/src/testing/mod.rs
@@ -1,4 +1,6 @@
 /// Implements some components of the renderer for testing purposes as the renderer crate is slowly fleshed out.
 mod indirect_pass;
+mod uniforms;
 
 pub use indirect_pass::IndirectPass;
+pub use uniforms::VertexUniform;
--- renderer/src/testing/uniforms.rs
+++ renderer/src/testing/uniforms.rs
@@ -1,5 +1,8 @@
+use bytemuck::{ Pod, Zeroable, };
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, PartialEq, Pod, Zeroable)]
-struct VertexUniform {
-	perspective_matrix: [f32; 16],
+pub struct VertexUniform {
+	pub perspective_matrix: [f32; 16],
+	pub view_matrix: [f32; 16],
 }
