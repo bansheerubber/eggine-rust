c6706f2dbc68b771f32b468d41dbbba38cc32dc5
bansheerubber <gary.cactus.email@gmail.com>
2023-02-16 22:59:32 -0700

    renderer: get diffuse shading pipeline fully working

--- renderer/src/testing/indirect_pass.rs
+++ renderer/src/testing/indirect_pass.rs
@@ -13,6 +13,7 @@ use super::VertexUniform;
 
 #[derive(Debug)]
 struct RenderTextures {
+	combination_uniform_bind_group: wgpu::BindGroup,
 	depth_texture: wgpu::Texture,
 	depth_view: wgpu::TextureView,
 	diffuse_format: wgpu::TextureFormat,
@@ -27,6 +28,7 @@ struct RenderTextures {
 #[derive(Debug)]
 pub struct IndirectPass {
 	blueprints: Vec<Rc<shape::Blueprint>>,
+	combination_program: Rc<Program>,
 	context: Rc<WGPUContext>,
 	/// Used for the `vertex_offset` for meshes in an indirect indexed draw call.
 	highest_vertex_offset: i32,
@@ -76,28 +78,32 @@ impl IndirectPass {
 			.allocate_node(8_000_000, 1, NodeKind::Buffer)
 			.unwrap();
 
-		// define shader names
-		let fragment_shader = "data/main.frag.spv".to_string();
-		let vertex_shader = "data/main.vert.spv".to_string();
+		// create the G-buffer generating program
+		let program = {
+			// define shader names
+			let fragment_shader = "data/main.frag.spv".to_string();
+			let vertex_shader = "data/main.vert.spv".to_string();
 
-		// lock shader table
-		let shader_table = boss.get_shader_table();
-		let mut shader_table = shader_table.write().unwrap();
+			// lock shader table
+			let shader_table = boss.get_shader_table();
+			let mut shader_table = shader_table.write().unwrap();
 
-		// load from carton
-		let fragment_shader = shader_table.load_shader_from_carton(&fragment_shader, carton).unwrap();
-		let vertex_shader = shader_table.load_shader_from_carton(&vertex_shader, carton).unwrap();
+			// load from carton
+			let fragment_shader = shader_table.load_shader_from_carton(&fragment_shader, carton).unwrap();
+			let vertex_shader = shader_table.load_shader_from_carton(&vertex_shader, carton).unwrap();
 
-		// create the program
-		let program = shader_table.create_program("main-shader", fragment_shader, vertex_shader);
+			// create the program
+			shader_table.create_program("main-shader", fragment_shader, vertex_shader)
+		};
 
+		// handle normals for G-buffer program
 		let uniforms_page = memory.new_page(5_000, wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST);
 		let page = memory.get_page_mut(uniforms_page).unwrap();
 		let vertex_uniform_buffer = page.allocate_node(
 			std::mem::size_of::<VertexUniform>() as u64, 4, NodeKind::Buffer
 		).unwrap();
 
-		// create uniform buffer bind groups
+		// create G-buffer uniform buffer bind groups
 		let uniform_bind_group = context.device.create_bind_group(&wgpu::BindGroupDescriptor {
 			entries: &[
 				wgpu::BindGroupEntry {
@@ -113,10 +119,32 @@ impl IndirectPass {
 			layout: program.get_bind_group_layouts()[0],
 		});
 
-		let render_textures = IndirectPass::create_render_textures(&context, boss.get_surface_config());
+		// create the G-buffer combination program
+		let combination_program = {
+			// define shader names
+			let fragment_shader = "data/combine.frag.spv".to_string();
+			let vertex_shader = "data/combine.vert.spv".to_string();
+
+			// lock shader table
+			let shader_table = boss.get_shader_table();
+			let mut shader_table = shader_table.write().unwrap();
+
+			// load from carton
+			let fragment_shader = shader_table.load_shader_from_carton(&fragment_shader, carton).unwrap();
+			let vertex_shader = shader_table.load_shader_from_carton(&vertex_shader, carton).unwrap();
+
+			// create the program
+			shader_table.create_program("combination-shader", fragment_shader, vertex_shader)
+		};
+
+		// create the G-buffer
+		let render_textures = IndirectPass::create_render_textures(
+			&context, boss.get_surface_config(), combination_program.clone()
+		);
 
 		IndirectPass {
 			blueprints: Vec::new(),
+			combination_program,
 			context,
 			highest_vertex_offset: 0,
 			indices_page: memory.new_page(24_000_000, wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST),
@@ -185,7 +213,7 @@ impl IndirectPass {
 	}
 
 	fn create_render_textures(
-		context: &WGPUContext, config: &wgpu::SurfaceConfiguration
+		context: &WGPUContext, config: &wgpu::SurfaceConfiguration, combination_program: Rc<Program>
 	) -> RenderTextures {
 		let depth_texture = context.device.create_texture(&wgpu::TextureDescriptor {
 			dimension: wgpu::TextureDimension::D2,
@@ -203,6 +231,8 @@ impl IndirectPass {
 				| wgpu::TextureUsages::RENDER_ATTACHMENT,
 			view_formats: &[],
 		});
+
+		let depth_view = depth_texture.create_view(&wgpu::TextureViewDescriptor::default());
 
 		let diffuse_format = config.format;
 		let diffuse_texture = context.device.create_texture(&wgpu::TextureDescriptor {
@@ -222,6 +252,8 @@ impl IndirectPass {
 			view_formats: &[],
 		});
 
+		let diffuse_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+
 		let normal_format = wgpu::TextureFormat::Bgra8Unorm; // TODO better format for this?
 		let normal_texture = context.device.create_texture(&wgpu::TextureDescriptor {
 			dimension: wgpu::TextureDimension::D2,
@@ -240,16 +272,65 @@ impl IndirectPass {
 			view_formats: &[],
 		});
 
-		RenderTextures {
-			depth_view: depth_texture.create_view(&wgpu::TextureViewDescriptor::default()),
-			diffuse_view: diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default()),
-			normal_view: normal_texture.create_view(&wgpu::TextureViewDescriptor::default()),
+		let normal_view = normal_texture.create_view(&wgpu::TextureViewDescriptor::default());
 
+		// create the samplers for the G-buffer
+		let diffuse_sampler = context.device.create_sampler(&wgpu::SamplerDescriptor {
+			label: None,
+			address_mode_u: wgpu::AddressMode::ClampToEdge,
+			address_mode_v: wgpu::AddressMode::ClampToEdge,
+			address_mode_w: wgpu::AddressMode::ClampToEdge,
+			mag_filter: wgpu::FilterMode::Linear,
+			min_filter: wgpu::FilterMode::Nearest,
+			mipmap_filter: wgpu::FilterMode::Nearest,
+			..Default::default()
+		});
+
+		let normal_sampler = context.device.create_sampler(&wgpu::SamplerDescriptor {
+			label: None,
+			address_mode_u: wgpu::AddressMode::ClampToEdge,
+			address_mode_v: wgpu::AddressMode::ClampToEdge,
+			address_mode_w: wgpu::AddressMode::ClampToEdge,
+			mag_filter: wgpu::FilterMode::Linear,
+			min_filter: wgpu::FilterMode::Nearest,
+			mipmap_filter: wgpu::FilterMode::Nearest,
+			..Default::default()
+		});
+
+		// create G-buffer combiner uniform buffer bind groups
+		let combination_uniform_bind_group = context.device.create_bind_group(&wgpu::BindGroupDescriptor {
+			entries: &[
+				wgpu::BindGroupEntry {
+					binding: 0,
+					resource: wgpu::BindingResource::TextureView(&diffuse_view),
+				},
+				wgpu::BindGroupEntry {
+					binding: 1,
+					resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+				},
+				wgpu::BindGroupEntry {
+					binding: 2,
+					resource: wgpu::BindingResource::TextureView(&normal_view),
+				},
+				wgpu::BindGroupEntry {
+					binding: 3,
+					resource: wgpu::BindingResource::Sampler(&normal_sampler),
+				},
+			],
+			label: None,
+			layout: combination_program.get_bind_group_layouts()[0],
+		});
+
+		RenderTextures {
+			combination_uniform_bind_group,
 			depth_texture,
+			depth_view,
 			diffuse_format,
 			diffuse_texture,
+			diffuse_view,
 			normal_format,
 			normal_texture,
+			normal_view,
 		}
 	}
 }
@@ -257,49 +338,61 @@ impl IndirectPass {
 /// Pass implementation. Indirectly render all shapes we have ownership over.
 impl Pass for IndirectPass {
 	fn states<'a>(&'a self) -> Vec<State<'a>> {
-		vec![State {
-			depth_stencil: Some(wgpu::DepthStencilState {
-				bias: wgpu::DepthBiasState::default(),
-				depth_write_enabled: true,
-				depth_compare: wgpu::CompareFunction::Less,
-				format: wgpu::TextureFormat::Depth32Float,
-				stencil: wgpu::StencilState::default(),
-			}),
-			program: &self.program,
-			render_targets: vec![
-				Some(self.render_textures.diffuse_format.into()),
-				Some(self.render_textures.normal_format.into()),
-			],
-			vertex_attributes: &[
-				wgpu::VertexBufferLayout { // vertices
-					array_stride: 4 * 3,
-					attributes: &[wgpu::VertexAttribute {
-						format: wgpu::VertexFormat::Float32x3,
-						offset: 0,
-						shader_location: 0,
-					}],
-					step_mode: wgpu::VertexStepMode::Vertex,
-				},
-				wgpu::VertexBufferLayout { // colors
-					array_stride: 4 * 3,
-					attributes: &[wgpu::VertexAttribute {
-						format: wgpu::VertexFormat::Float32x3,
-						offset: 0,
-						shader_location: 1,
-					}],
-					step_mode: wgpu::VertexStepMode::Vertex,
-				},
-				wgpu::VertexBufferLayout { // normals
-					array_stride: 4 * 3,
-					attributes: &[wgpu::VertexAttribute {
-						format: wgpu::VertexFormat::Float32x3,
-						offset: 0,
-						shader_location: 2,
-					}],
-					step_mode: wgpu::VertexStepMode::Vertex,
-				},
-			],
-		}]
+		vec![
+			State {
+				depth_stencil: Some(wgpu::DepthStencilState {
+					bias: wgpu::DepthBiasState::default(),
+					depth_write_enabled: true,
+					depth_compare: wgpu::CompareFunction::Less,
+					format: wgpu::TextureFormat::Depth32Float,
+					stencil: wgpu::StencilState::default(),
+				}),
+				program: &self.program,
+				render_targets: vec![
+					Some(self.render_textures.diffuse_format.into()),
+					Some(self.render_textures.normal_format.into()),
+				],
+				vertex_attributes: &[
+					wgpu::VertexBufferLayout { // vertices
+						array_stride: 4 * 3,
+						attributes: &[wgpu::VertexAttribute {
+							format: wgpu::VertexFormat::Float32x3,
+							offset: 0,
+							shader_location: 0,
+						}],
+						step_mode: wgpu::VertexStepMode::Vertex,
+					},
+					wgpu::VertexBufferLayout { // colors
+						array_stride: 4 * 3,
+						attributes: &[wgpu::VertexAttribute {
+							format: wgpu::VertexFormat::Float32x3,
+							offset: 0,
+							shader_location: 1,
+						}],
+						step_mode: wgpu::VertexStepMode::Vertex,
+					},
+					wgpu::VertexBufferLayout { // normals
+						array_stride: 4 * 3,
+						attributes: &[wgpu::VertexAttribute {
+							format: wgpu::VertexFormat::Float32x3,
+							offset: 0,
+							shader_location: 2,
+						}],
+						step_mode: wgpu::VertexStepMode::Vertex,
+					},
+				],
+			},
+			State {
+				depth_stencil: None,
+				program: &self.combination_program,
+				render_targets: vec![Some(wgpu::ColorTargetState {
+					blend: None,
+					format: wgpu::TextureFormat::Bgra8UnormSrgb,
+					write_mask: wgpu::ColorWrites::ALL,
+				})],
+				vertex_attributes: &[],
+			},
+		]
 	}
 
 	fn encode(
@@ -333,7 +426,7 @@ impl Pass for IndirectPass {
 			.unwrap()
 			.write_buffer(&self.indirect_command_buffer_node, &buffer);
 
-		// handle the render pass stuff
+		// render to the G-buffer
 		{
 			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
 				color_attachments: &[
@@ -343,7 +436,7 @@ impl Pass for IndirectPass {
 							store: true,
 						},
 						resolve_target: None,
-						view: &view,
+						view: &self.render_textures.diffuse_view,
 					}),
 					Some(wgpu::RenderPassColorAttachment {
 						ops: wgpu::Operations {
@@ -384,6 +477,7 @@ impl Pass for IndirectPass {
 				2, memory.get_page(self.normals_page).unwrap().get_buffer().slice(0..self.vertices_page_written)
 			);
 
+			// bind uniforms
 			render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
 
 			// draw all the objects
@@ -391,13 +485,40 @@ impl Pass for IndirectPass {
 				memory.get_page(self.indirect_command_buffer).unwrap().get_buffer(), 0, draw_call_count
 			);
 		}
+
+		// combine the textures in the G-buffer
+		{
+			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+				color_attachments: &[
+					Some(wgpu::RenderPassColorAttachment {
+						ops: wgpu::Operations {
+							load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+							store: true,
+						},
+						resolve_target: None,
+						view: &view,
+					}),
+				],
+				depth_stencil_attachment: None,
+				label: None,
+			});
+
+			render_pass.set_pipeline(pipelines[1]);
+
+			// bind uniforms
+			render_pass.set_bind_group(0, &self.render_textures.combination_uniform_bind_group, &[]);
+
+			render_pass.draw(0..3, 0..1);
+		}
 	}
 
 	fn resize(&mut self, config: &wgpu::SurfaceConfiguration) {
 		self.window_height = config.height;
 		self.window_width = config.width;
 
-		self.render_textures = IndirectPass::create_render_textures(&self.context, config);
+		self.render_textures = IndirectPass::create_render_textures(
+			&self.context, config, self.combination_program.clone()
+		);
 	}
 }
 
