2e6091c2fe5271b07b92875086b48967b777b68c
bansheerubber <gary.cactus.email@gmail.com>
2023-03-05 16:48:31 -0700

    renderer: remove FBX loader

--- renderer/src/shape/blueprint/mod.rs
+++
@@ -1,9 +0,0 @@
-mod blueprint;
-mod mesh;
-mod state;
-
-pub use blueprint::Blueprint;
-pub use blueprint::BlueprintDataKind;
-pub use blueprint::BlueprintError;
-pub use mesh::Mesh;
-pub use state::BlueprintState;
--- renderer/src/shape/mod.rs
+++ renderer/src/shape/mod.rs
@@ -1,5 +1,4 @@
 mod batch_parameters;
-mod blueprint;
 pub mod blueprint2;
 mod shape;
 mod triangulator;
@@ -9,10 +8,5 @@ pub type IndexType = u32;
 
 pub(crate) use batch_parameters::BatchParameters;
 pub(crate) use batch_parameters::BatchParametersKey;
-pub use blueprint::Blueprint;
-pub use blueprint::BlueprintDataKind;
-pub use blueprint::BlueprintError;
-pub use blueprint::BlueprintState;
-pub use blueprint::Mesh;
 pub use shape::Shape;
 pub use triangulator::triangulator;
